89e558b9952cc76078c9d2d43a18367e76c9180e
AfazPaul1 <afazpaul@gmail.com>
2025-06-07 17:28:27 +0530

    feat(secureShuffle): shuffle function and set up state body: implemented a cryptographically secure fisher-yates shuffle function. used node crypto module's getrandomvalues method. it fills the array(uint32array) passed to it with random values. needs a typed array. then we loop over the array we want to shuffle taking i=length-1 and decrementing. at each loop we modulo the randomvalue at the same index with index+1. the remainder is j. we do array destructuring assignment with indices i and j and swap. then return the array. its done inplace.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,47 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
- 
-
-  interface Card {
+interface Card {
     id:string,
     value: string,
     isFlipped: boolean,
     isMatched: boolean
   }
 
-  const values = ["cat", "dog"]
+const values = ["cat", "dog"]
 
-  let cardList:Card[] = []
+function secureShuffle(arr:Card[]):Card[] {
+    //uint32 for cryptographically strong shuffles
+    const randomValues = new Uint32Array(arr.length)
+    crypto.getRandomValues(randomValues)
+    console.log(randomValues);
+    console.log([...arr]);
+    
+    for (let i = arr.length-1; i > 0; i--) {
+      const j = randomValues[i] % (i+1);
+      console.log(randomValues[i], i+1,i, j);
+      
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    console.log(arr);
+    
+
+    return arr
+  }  
+
+function App() {
+ 
+  const [cards, setCards] = useState<Card[]>([])
 
   const createDeck = () => {
-    //tried using map initially but realised i could only map over that particular value and return but i have to return 2 values in the array for each value
-    //decided to use a for loop
-    //flatMap
-    //but later learned about flatMap
-    //It’s a method that maps over an array and flattens the result by one level.
-    //flat()
-    //its an array method which creates a new array with all sub array elements concatenated into it recursively upto a depth
-    //flat(1) does [0, 1, [2, [3, [4, 5]]]]; - [0, 1, 2, Array [3, Array [4, 5]]]
-    //use flatmap if for every element we want n element in the transformed array
-
-    cardList = values.flatMap((value) => [
+    //removed flatmap note
+    return secureShuffle(values.flatMap((value) => [
       {id:crypto.randomUUID(), value, isFlipped: false, isMatched:false},
       {id:crypto.randomUUID(), value, isFlipped: false, isMatched:false},
-    ])
+    ]))
   }
   createDeck()
-  console.log(cardList);
   
-  // for (let i = 0; i < values.length*2; i++) {
-  //   cardList.push({
-  //     id: i,
-  //     //how do i assign value? this is a struggle? have to write some complicated logic
-  //     value, 
-  //     isFlipped: false,
-  //     isMatched:false
-  //   })
-  // }
-
-
 
   return (
     <>
