d115c0d9d241ba72f523e47b23d9da73faf5f5b6
AfazPaul1 <afazpaul@gmail.com>
2025-06-07 16:15:51 +0530

    feat(createDeck): body: created Card interface with id, a value to display, a isMatched boolean to track whether its matched at which we dont allow flipping and show its value permanently. a isFlipped prop which when true we show its value and if false we hide the value. we initialize cardList which is a list of cards. used a flatmap to create 2 cards for every value.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,47 @@ import './App.css'
 function App() {
  
 
- 
+  interface Card {
+    id:string,
+    value: string,
+    isFlipped: boolean,
+    isMatched: boolean
+  }
+
+  const values = ["cat", "dog"]
+
+  let cardList:Card[] = []
+
+  const createDeck = () => {
+    //tried using map initially but realised i could only map over that particular value and return but i have to return 2 values in the array for each value
+    //decided to use a for loop
+    //flatMap
+    //but later learned about flatMap
+    //It’s a method that maps over an array and flattens the result by one level.
+    //flat()
+    //its an array method which creates a new array with all sub array elements concatenated into it recursively upto a depth
+    //flat(1) does [0, 1, [2, [3, [4, 5]]]]; - [0, 1, 2, Array [3, Array [4, 5]]]
+    //use flatmap if for every element we want n element in the transformed array
+
+    cardList = values.flatMap((value) => [
+      {id:crypto.randomUUID(), value, isFlipped: false, isMatched:false},
+      {id:crypto.randomUUID(), value, isFlipped: false, isMatched:false},
+    ])
+  }
+  createDeck()
+  console.log(cardList);
+  
+  // for (let i = 0; i < values.length*2; i++) {
+  //   cardList.push({
+  //     id: i,
+  //     //how do i assign value? this is a struggle? have to write some complicated logic
+  //     value, 
+  //     isFlipped: false,
+  //     isMatched:false
+  //   })
+  // }
+
+
 
   return (
     <>
