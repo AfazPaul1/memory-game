68ca2803801d31498b0b12d8d3d25b1f887f62cc
AfazPaul1 <afazpaul@gmail.com>
2025-06-09 00:31:25 +0530

    feat(game logic): body: i want to perform some state updating logic when the length of my flipped is 2. since its some state updation we can do while rendering. have to do it as an effect. the dependecy is the flipped state. though we only care if its length is 2. first we lockboard and prevent clicks from updating state. next we check whether the values in the 2 elements in flipped are same, if yes then we update cards and set those 2 cards isMatched to true. this makes them stay flipped. actually i dont think i need this now. could do the same with flipped. then we empty flipped and flip lockboard. if flipped card's values are not same then we set both cards flipped to false, reset board and lockboard

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,9 @@ function App() {
       })
   const [flipped, setFlipped] = useState<Card[]>([])
   //tracks the currently flipped cards
-   
+   const [lockBoard, setLockBoard] = useState(false)
    const handleCardClick = (clickedCard: Card) => {
-    if (clickedCard.isFlipped || clickedCard.isMatched) {
+    if (clickedCard.isFlipped || clickedCard.isMatched || lockBoard) {
       return
     } 
     //card.isFlipped = true
@@ -53,12 +53,43 @@ function App() {
     setFlipped([...flipped, clickedCard])
    }
 
+   useEffect(() => {
+    if(flipped.length === 2){
+      setLockBoard(true)
+      if(flipped[0].value === flipped[1].value) {
+          const value = flipped[0].value
+          setCards(cards?.map(card => {
+          if (card.value === value) {
+            return {...card, isMatched: true}
+          } else {
+            return card
+          }
+          }))
+          setFlipped([])
+          setLockBoard(false)
+      }
+      else {
+        setTimeout(() => {
+          setCards(cards?.map(card => {
+          if (card.id === flipped[0].id || flipped[1].id) {
+            return {...card, isFlipped:false}
+          } else {
+            return card
+          }
+        }))
+        setFlipped([])
+        setLockBoard(false)
+        }, 2000)
+        
+      }
+    }
+   }, [flipped])
 
   const content = cards?.map((card) =>  <Card key={card.id} onClick={() => handleCardClick(card)} value={card.value} isFlipped={card.isFlipped} isMatched={card.isMatched}  />)
   
   return (
     <>
-      <div className='grid grid-cols-4 gap-3 p-3 justify-items-center' >
+      <div className='grid grid-cols-4 gap-3 p-3 justify-items-center align-items-center' >
         {content}
       </div>
     </>
