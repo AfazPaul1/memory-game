eccfd8da8c8d68e8c458c25b9175ba0a13fdc439
AfazPaul1 <afazpaul@gmail.com>
2025-06-09 15:12:14 +0530

    fix: wrapped Card with React.memo to prevent rerender body: in card component we track everytime it rerender. we see that when a single card is clicked and app rerenders and if parent rerenders child rerenders. so every card rerenders. to prevent that we memoize the Card. when we memoize it it checks the prop values. if they are same then rerender is skipped.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 interface propTypes {
     value: string,
     isFlipped: boolean,
@@ -5,13 +6,15 @@ interface propTypes {
     onClick: () => void,
     
 }
+let renderCount = 0
 
-
-export function Card({value, isFlipped, isMatched, onClick,} : propTypes) {
-
+export const Card = React.memo(function Card({value, isFlipped, isMatched, onClick,} : propTypes) {
+    renderCount++
+    console.log('rendering', renderCount);
+    
     return (
         <button onClick={onClick}  className="w-[64px] h-[64px] border-2 bg-sky-500 hover:bg-sky-700">
             {isFlipped || isMatched ? value : ""}
         </button>
     )
-}
+})
